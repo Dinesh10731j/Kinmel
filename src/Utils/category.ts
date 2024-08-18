ec8c5c12ad221c092c585c49b19a076570fe7a9d
Dinesh10731j <dinesh1122@gmail.com>
2024-08-18 11:49:27 +0545

    categores section is completed  and changes some design

--- src/Utils/category.ts
+++ src/Utils/category.ts
@@ -1,23 +1,57 @@
-export const Category = [{
-    category:"Women Fashion",
-},{
-    category:"Men's Fashion",
-},{
-category:"Electronics",
+import {Smartphone,Laptop,Headphones,Camera,GamepadIcon} from "lucide-react"
+export const Category = [
+  {
+    category: "Women Fashion",
+  },
+  {
+    category: "Men's Fashion",
+  },
+  {
+    category: "Electronics",
+  },
+  {
+    category: "Home & LifeStyle",
+  },
+  {
+    category: "Medicine",
+  },
+  {
+    category: "Sports & Outdoor",
+  },
+  {
+    category: "Baby's & Toys",
+  },
+  {
+    category: "Groceries & Pets",
+  },
+  {
+    category: "Health & Beauty",
+  },
+];
 
-},{
-    category:"Home & LifeStyle",
-
-},{
-    category:"Medicine",
-},{
-    category:"Sports & Outdoor",
-
-},{
-    category:"Baby's & Toys",
-},{
-    category:"Groceries & Pets"
-},{
-    category:"Health & Beauty"
-}]
-
+export const BrowserByCategory = [
+  {
+    name: "Iphone",
+    icon:Smartphone
+  },
+  {
+    name: "Laptop",
+    icon:Laptop
+  },
+  {
+    name: "Smartphone",
+    icon:Smartphone
+  },
+  {
+    name: "Cameras",
+    icon:Camera
+  },
+  {
+    name: "HeadPhones",
+    icon:Headphones
+  },
+  {
+    name: "Gaming",
+    icon:GamepadIcon
+  },
+];
