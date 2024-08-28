938537b77dfcc9f064f42c036df78e735efb8379
Dinesh10731j <dinesh1122@gmail.com>
2024-08-28 11:46:00 +0545

    cart section completed

--- src/Landingpage/Home.tsx
+++ src/Landingpage/Home.tsx
@@ -24,7 +24,7 @@ ShieldCheck,
 } from "lucide-react";
 
 interface HomeProps {
-  handleAddCart: (id: string,img:string) => void;
+  handleAddCart: (id: string,img:string,title:string,price:number) => void;
 }
 
 
@@ -225,7 +225,7 @@ const Home: React.FC<HomeProps> = ({ handleAddCart }) => {
                 <button className="text-[#DB4444] hover:text-red-600">
                   <Eye />
                 </button>
-                <button onClick={()=>handleAddCart(product?.id,product?.image)} className="bg-[#DB4444] text-white px-4 py-2 rounded-lg hover:bg-[#b73333] flex items-center gap-2">
+                <button onClick={()=>handleAddCart(product?.id,product?.image,product?.title,product?.price)} className="bg-[#DB4444] text-white px-4 py-2 rounded-lg hover:bg-[#b73333] flex items-center gap-2">
                   <ShoppingCart  />
                   Add to Cart
                 </button>
