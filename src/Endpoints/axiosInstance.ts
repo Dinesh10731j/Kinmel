e14a89453503866b3401a58ab3cb3a89bcad5415
Dinesh10731j <dinesh1122@gmail.com>
2024-09-02 22:40:42 +0545

    some changes in the design

--- src/Endpoints/axiosInstance.ts
+++ src/Endpoints/axiosInstance.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:2081',
+  baseURL: 'https://kinmel-api.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
