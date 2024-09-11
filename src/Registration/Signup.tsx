4096a3e7a6a7b6cc37b1fc865e182309ef31add6
Dinesh10731j <dinesh1122@gmail.com>
2024-09-11 11:39:12 +0545

    name field is added in signup page

--- src/Registration/Signup.tsx
+++ src/Registration/Signup.tsx
@@ -11,6 +11,7 @@ import { CircularProgress } from "@mui/material";
 
 
 interface SignupDetails {
+  name:string
   email: string;
   password: string;
   confirmpassword: string;
@@ -46,6 +47,18 @@ const Signup = () => {
         <h2 className="text-2xl font-bold mb-6">Create an Account</h2>
 
         <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-sm">
+        <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Name
+            </label>
+            <input
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Enter your name"
+              {...register("name", { required: "Name is required" })}
+            />
+            {errors.name && <p className="text-red-500 text-sm  mt-2  italic">{errors.name.message}</p>}
+          </div>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">
               Email
