681fc979c4bfaa579a28c3acccd3f783fb7d543b
Dinesh10731j <dinesh1122@gmail.com>
2024-12-09 13:41:05 +0545

    e-sewa intregation completed

--- src/hooks/E-sewaIntregation.ts
+++ src/hooks/E-sewaIntregation.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { useMutation } from '@tanstack/react-query';
 import { useSelector } from 'react-redux';
 
-// Function for payment initiation
 const e_sewaIntegration = async (products: any[]) => {
   const totalPrice = products.reduce((accumulator: number, product: { price: number; quantity: number }) => {
     return accumulator + (product.price * product.quantity); 
@@ -25,13 +24,11 @@ const e_sewaIntegration = async (products: any[]) => {
   }
 };
 
-// Custom hook for eSewa payment integration
 export const useESewaPayment = () => {
-  // `useSelector` should be inside a component or custom hook, not a regular function
   const products = useSelector((state: any) => state.product);
 
   return useMutation({
     mutationKey: ['E-Sewa'],
-    mutationFn: () => e_sewaIntegration(products), // Pass products as an argument to the integration function
+    mutationFn: () => e_sewaIntegration(products), 
   });
 };
