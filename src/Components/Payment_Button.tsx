139be263e665fe1d91144e9f447079556e4b2a80
Dinesh10731j <dinesh1122@gmail.com>
2024-12-11 16:17:27 +0545

    payment error fixed

--- src/Components/Payment_Button.tsx
+++ src/Components/Payment_Button.tsx
@@ -11,6 +11,8 @@ const E_SewaPaymentButton = () => {
 
   });
 
+  const totalAmount = products.reduce((acc:any, item:any) => acc + item.price * item.quantity, 0);
+
 
   const handlePayment = () => {
     mutation.mutate();
@@ -22,7 +24,7 @@ const E_SewaPaymentButton = () => {
       disabled={mutation.isPending}
       className={`bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600
         
-         ${products?.length <= 0 ? "cursor-not-allowed" : "cursor-pointer"}
+      ${totalAmount <= 0 ? 'cursor-not-allowed opacity-50' : 'cursor-pointer hover:bg-[#c33333]'}
         `}
     >
       {mutation.isPending? 'Processing...' : 'Pay with eSewa'}
